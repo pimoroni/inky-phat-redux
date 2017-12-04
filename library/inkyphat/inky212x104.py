c94928deec594a7a844a73d8f071970b28d1f502
sandyjmacdonald <sandyjmacdonald@gmail.com>
2017-12-04 16:54:15 +0000

    Tweaked colour tables.

--- library/inkyphat/inky212x104.py
+++ library/inkyphat/inky212x104.py
@@ -228,9 +228,9 @@ class Inky212x104:
 #       A     B     C     D   |
         67,   10,   31,   10,    4,  # 0 Flash
         16,   8,    4,    4,     6,  # 1 clear
-        8,    8,    9,    37,    10,  # 2 bring in the black
-        8,    8,    9,    67,    10, # 3 time for red
-        8,    16,   8,    8,     4,  # 4 final black sharpen phase
+        8,    8,    8,    37,    10,  # 2 bring in the black
+        8,    8,    8,    72,    16, # 3 time for red
+        6,    6,    6,    2,     2,  # 4 final black sharpen phase
         0,    0,    0,    0,     0,  # 5
         0,    0,    0,    0,     0,  # 6
         0,    0,    0,    0,     0   # 7
